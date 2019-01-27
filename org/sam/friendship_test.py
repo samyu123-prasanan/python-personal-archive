77b0623ff05344cd27c69fb33b7e9bbb3b20f412
Samyuktha Prasanan <wordbay99@gmail.com>
2019-01-27 19:18:11 +0530

    alias function added

--- org/sam/friendship_test.py
+++ org/sam/friendship_test.py
@@ -1,13 +1,17 @@
 
-import random
+import random as r
 
+'''
+    This function will predict your friendship meter
+'''
 def get_friendship_meter(name1, name2):
-    print('test')
+    #print('test')
     
     print('Your Name : ', name1)
     print("Your friend Name : ", name2)
     
-    meter = random.randint(1, 100)
+    meter = r.randint(1, 100) 
+    
         
     print("Your friendship meter : ", meter)
     
