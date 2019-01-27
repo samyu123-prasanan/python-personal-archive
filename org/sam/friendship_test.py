ce74fe55e6029945dec14e886f7248395224a87d
Samyuktha Prasanan <wordbay99@gmail.com>
2019-01-27 19:10:23 +0530

    friendship meter function added

---
+++ org/sam/friendship_test.py
@@ -0,0 +1,15 @@
+
+import random
+
+def get_friendship_meter(name1, name2):
+    print('test')
+    
+    print('Your Name : ', name1)
+    print("Your friend Name : ", name2)
+    
+    meter = random.randint(1, 100)
+        
+    print("Your friendship meter : ", meter)
+    
+    
+get_friendship_meter("Purnima", "Sam")    
