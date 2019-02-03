10eccfbfbbe756d843cf7b67f9a2fccf1228ff13
Samyuktha Prasanan <wordbay99@gmail.com>
2019-02-03 19:09:37 +0530

    sleep added

---
+++ org/sam/sleep.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+
+Course work: 
+
+@author: Samyuktha
+
+Source:
+    
+'''
+
+# Import necessary modules
+ 
+
+
+
+
+import time
+
+def startpy():
+    
+    i=0
+    while (i<5): 
+        print ("Hello Toronto")
+        time.sleep(5)
+        i = i+1
+    #print ("Bubye")
+
+
+if __name__ == '__main__':
+    startpy()
