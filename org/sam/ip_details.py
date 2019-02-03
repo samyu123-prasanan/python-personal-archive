d3932939e2f8b95ee3e535abef7575991943d00e
Samyuktha Prasanan <wordbay99@gmail.com>
2019-02-03 18:48:49 +0530

    ip details added

---
+++ org/sam/ip_details.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+
+Course work: 
+
+@author: raja
+
+Source:
+    
+'''
+
+# Import necessary modules
+from IPy import IP
+ 
+
+
+
+def startpy(ip_input):
+    ip = IP(ip_input)
+    details = ip.iptype()
+    #print (ip)
+    print(details)
+
+
+if __name__ == '__main__':
+    startpy("192.168.1.5")
